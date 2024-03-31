fa78ec3fe0aa05d957964e9c7fecbd196808ced9
Aryan <aaryansharma.1210@gmail.com>
2024-04-01 00:34:46 +0530

    Setup dev configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {},
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'istockphoto.comm',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'upload.wikimedia.org',
+          port: '',
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
